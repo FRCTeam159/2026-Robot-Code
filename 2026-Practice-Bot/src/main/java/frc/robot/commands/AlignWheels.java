c030f677778bff2ad9d3dad969987b1244b8b4c4
dwsindorf <dean.sindorf@gmail.com>
2026-02-14 12:02:39 -0700

    added untracked files

---
+++ 2026-Practice-Bot/src/main/java/frc/robot/commands/AlignWheels.java
@@ -0,0 +1,67 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.SwerveModule;
+
+public class AlignWheels extends Command {
+  private final Timer m_timer = new Timer();
+  private final Drivetrain m_drive;
+  double timeout;
+  boolean done=false;
+  /** Creates a new AlignWheels. */
+  public AlignWheels(Drivetrain drive, double tm) {
+    m_drive=drive;
+    timeout=tm;
+    m_timer.start();
+    addRequirements(drive);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    SwerveModule.optimize_enabled = false;
+    m_drive.resetWheels(true);
+    m_timer.reset();
+    done=false;
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if(!m_drive.wheelsReset()){
+     // m_drive.resetWheels(false);
+      m_drive.drive(0.00, 0, 0, false);
+    }
+    else
+      done=true;     
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_drive.reset();
+    m_drive.resetOdometry();
+    m_drive.drive(0.001,0,0,false);
+    if(done) {
+      System.out.println(" - Wheels are aligned");
+    } else {
+      System.out.println(" - Timeout expired");
+    }
+    SwerveModule.optimize_enabled = true;
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+  if(done || m_timer.get()>timeout) 
+  //if(m_timer.get()>timeout)   
+     return true;
+    return false;
+  }
+}
